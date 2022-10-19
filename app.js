935615783db6db86f7374b6807b7347cfedf1e93
Maria Dolores Santini Romero <s8romeromariadolores@gmail.com>
2022-10-19 11:57:14 -0500

    Actualizando Buttons -03

--- app.js
+++ app.js
@@ -9,18 +9,19 @@ const loadData = (url, page = 1) => {
         const personajes = respJson.results;
         console.log(info.next);
         console.log(info.prev);
-        if(!info.prev){
-            document.querySelector('#prev').classList.add('disabled')
-        } else {
-            document.querySelector('#prev').classList.remove('disabled')
-            document.querySelector('#prev').setAttribute('data-id', page - 1)
-        }
-        if(!info.next){
-            document.querySelector('#next').classList.add('disabled')
-        } else {
-            document.querySelector('#next').classList.remove('disabled')
-            document.querySelector('#next').setAttribute('data-id', page + 1)
-        }
+        creaButtons();
+          //if(!info.prev){
+          //    document.querySelector('#prev').classList.add('disabled')
+          //} else {
+          //    document.querySelector('#prev').classList.remove('disabled')
+          //    //document.querySelector('#prev').setAttribute('data-id', page - 1)
+          //}
+          //if(!info.next){
+          //    document.querySelector('#next').classList.add('disabled')
+          //} else {
+          //    document.querySelector('#next').classList.remove('disabled')
+          //    //document.querySelector('#next').setAttribute('data-id', page + 1)
+          //}
         console.log(personajes);
         showCharacters(personajes);
     })
@@ -35,34 +36,48 @@ const navegacion = (e) => {
 }
   loadData(urlBase);
 
-document.querySelector('.botones').addEventListener('click', navegacion);
+//document.querySelector('.botones').addEventListener('click', navegacion);
 
 const showCharacters = (personajes) => {
-  /*  const ul = document.createElement('ul');
-    personajes.forEach(personaje => {
-        const li = document.createElement('li');
-        li.innerHTML = `EL personaje ${personaje.name} is ${personaje.status}
-                        <img src="${personaje.image}">`;
-        ul.appendChild(li);
-    });
-    document.querySelector('#respuesta').appendChild(ul);*/
     const contenedorRespuesta = document.querySelector('#respuesta');
+    while(contenedorRespuesta.firstChild){
+      contenedorRespuesta.remove(contenedorRespuesta.firstChild);
+    }
     personajes.forEach(personaje => {
         contenedorRespuesta.appendChild(creaCard(personaje));
-    });
+    })
 }
 
 const creaCard = (personaje) => {
     const card = document.createElement('div');
+    card.style = 'float: left';
     const html = `
     <div class="card m-3" style="width: 18rem; float: left;">
-    <img src="${personaje.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${personaje.name}</h5>
-      <p class="card-text">${personaje.status}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
+      <img src="${personaje.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${personaje.name}</h5>
+        <p class="card-text">${personaje.status}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
   </div>`;
     card.innerHTML = html;
   return card;
+}
+
+//<button id="prev" class="btn btn-success btn-lg" data-id="">Anterior</button>
+//<button id="next" class="btn btn-success btn-lg" data-id="">Siguiente</button>
+const creaButtons = () => {
+  const contenedorButtons = document.querySelector('#botones');
+  contenedorButtons.innerText = '';
+  const btnPrev = document.createElement('button');
+  btnPrev.id = 'prev';
+  btnPrev.className = 'btn btn-success btn-lg';
+  btnPrev.innerText = 'Anterior';
+  contenedorButtons.appendChild(btnPrev);
+  const btnNext = document.createElement('button');
+  btnNext.id = 'next';
+  btnNext.className = 'btn btn-success btn-lg';
+  btnNext.innerText = 'Siguiente';
+  contenedorButtons.appendChild(btnNext);
+  
 }
