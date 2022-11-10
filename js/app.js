82eea25e9b508d7ab8a46f5c1df378c0fb8c7903
Maria Dolores Santini Romero <s8romeromariadolores@gmail.com>
2022-11-10 13:22:15 -0600

    Finalización del proyecto

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import { showModal, navegacion, loadData} from './eventos.js';
+  
+loadData(urlBase);
+document.querySelector()
+document.querySelector('.botones').addEventListener('click', navegacion);
